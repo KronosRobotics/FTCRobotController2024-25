ed8c9b1a687d48c9418062e889de82eb62e4f3c0
Aryan A <aryananumula@gmail.com>
2024-10-09 22:47:24 -0700

    add placeholders for BlockPipeline, ClipPipeline created pipelines folder

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/BlockPipeline.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.pipelines;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class BlockPipeline extends OpenCvPipeline {
+    //TODO: Actually implement once Sid gets blocks
+    @Override
+    public Mat processFrame(Mat inputMat) {
+        // Your code here
+        return inputMat;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/ClipPipeline.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.pipelines;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class ClipPipeline extends OpenCvPipeline {
+    //TODO: Actually implement once Sid gets clips
+    @Override
+    public Mat processFrame(Mat inputMat) {
+        // Your code here
+        return inputMat;
+    }
+
+}
