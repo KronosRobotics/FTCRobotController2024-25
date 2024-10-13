47d76bf96014bdf51b9196fac023cfe63d120aa9
Aryan A <aryananumula@gmail.com>
2024-10-12 21:27:55 -0700

    fixed everything

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/InitAutonomousPipeline.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.pipelines;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.TankDrive;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public class InitAutonomousPipeline extends LinearOpMode{
+    OpenCvWebcam webcam;
+    OpenCvCamera camera;
+    BlockPipeline bp;
+
+    @Override
+    public void runOpMode() {
+        //TODO: Finish this later
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        webcam.setPipeline(bp);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
+        while (!isStarted() && !isStopRequested())
+        {
+            telemetry.addData("analysis", bp.getAnalysis());
+            telemetry.update();
+        }
+    }
+}
