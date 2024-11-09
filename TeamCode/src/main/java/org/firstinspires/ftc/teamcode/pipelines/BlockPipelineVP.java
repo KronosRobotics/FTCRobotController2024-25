0de5b08e572fb2082842cc5105f297fff7086a7b
Aryan A <aryananumula@gmail.com>
2024-11-09 09:54:51 -0800

    add tensorflow stuff (ima crash out)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/BlockPipelineVP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/BlockPipelineVP.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode.pipelines;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.os.Trace;
 import android.text.TextPaint;
 
 import androidx.annotation.NonNull;
@@ -21,14 +22,18 @@ import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibra
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
+import org.tensorflow.lite.Interpreter;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Vector;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.DoubleSupplier;
+import java.util.Map;
 
 public class BlockPipelineVP implements VisionProcessor, CameraStreamSource {
     private DoubleSupplier minArea;
@@ -43,12 +48,42 @@ public class BlockPipelineVP implements VisionProcessor, CameraStreamSource {
     private Mat sel1 = new Mat(); // these facilitate capturing through 0
     private Mat sel2 = new Mat();
     private AtomicReference<Bitmap> lastFrame = new AtomicReference<>(Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565));
-    // private TfodProcessor tfodProcessor;
     private VisionPortal visionPortal;
     private final boolean USE_WEBCAM = true;
 
+    // Only return this many results.
+    private static final int NUM_DETECTIONS = 10;
+    // Float model
+    private static final float IMAGE_MEAN = 128.0f;
+    private static final float IMAGE_STD = 128.0f;
+    // Number of threads in the java app
+    private static final int NUM_THREADS = 4;
+    private boolean isModelQuantized;
+    // Config values.
+    private int inputSize;
+    // Pre-allocated buffers.
+    private Vector<String> labels = new Vector<String>();
+    private int[] intValues;
+    // outputLocations: array of shape [Batchsize, NUM_DETECTIONS,4]
+    // contains the location of detected boxes
+    private float[][][] outputLocations;
+    // outputClasses: array of shape [Batchsize, NUM_DETECTIONS]
+    // contains the classes of detected boxes
+    private float[][] outputClasses;
+    // outputScores: array of shape [Batchsize, NUM_DETECTIONS]
+    // contains the scores of detected boxes
+    private float[][] outputScores;
+    // numDetections: array of shape [Batchsize]
+    // contains the number of detected boxes
+    private float[] numDetections;
 
-    public void ColourMassDetectionProcessor(@NonNull Scalar lower, @NonNull Scalar upper, DoubleSupplier minArea, DoubleSupplier left, DoubleSupplier right){
+    private ByteBuffer imgData;
+
+    private Interpreter tfLite;
+
+
+    public void ColourMassDetectionProcessor(@NonNull Scalar lower, @NonNull Scalar upper, DoubleSupplier minArea,
+            DoubleSupplier left, DoubleSupplier right) {
         this.blocks = new ArrayList<>();
         this.lower = lower;
         this.upper = upper;
@@ -73,21 +108,7 @@ public class BlockPipelineVP implements VisionProcessor, CameraStreamSource {
         linePaint.setStrokeJoin(Paint.Join.ROUND);
     }
 
-    private void telemetryTfod() {
-
-        //List<Recognition> currentRecognitions = tfod.getRecognitions();
-        //telemetry.addData("# Objects Detected", currentRecognitions.size());
-
-        // Step through the list of recognitions and display info for each one.
-        /*for (Recognition recognition : currentRecognitions) {
-            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
-            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
-
-            telemetry.addData(""," ");
-            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
-            telemetry.addData("- Position", "%.0f / %.0f", x, y);
-            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
-        }   // end for() loop*/
+    public void getBounds(Mat mat) {
 
     }
 
@@ -96,22 +117,88 @@ public class BlockPipelineVP implements VisionProcessor, CameraStreamSource {
 
     }
 
+    public ArrayList<Object> recognizeImage(final Bitmap bitmap) {
+        // Log this method so that it can be analyzed with systrace.
+        Trace.beginSection("recognizeImage");
+
+        Trace.beginSection("preprocessBitmap");
+        // Preprocess the image data from 0-255 int to normalized float based
+        // on the provided parameters.
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        imgData.rewind();
+        for (int i = 0; i < inputSize; ++i) {
+            for (int j = 0; j < inputSize; ++j) {
+                int pixelValue = intValues[i * inputSize + j];
+                if (isModelQuantized) {
+                    // Quantized model
+                    imgData.put((byte) ((pixelValue >> 16) & 0xFF));
+                    imgData.put((byte) ((pixelValue >> 8) & 0xFF));
+                    imgData.put((byte) (pixelValue & 0xFF));
+                } else { // Float model
+                    imgData.putFloat((((pixelValue >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                    imgData.putFloat((((pixelValue >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                    imgData.putFloat(((pixelValue & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                }
+            }
+        }
+        Trace.endSection(); // preprocessBitmap
+
+        // Copy the input data into TensorFlow.
+        Trace.beginSection("feed");
+        outputLocations = new float[1][NUM_DETECTIONS][4];
+        outputClasses = new float[1][NUM_DETECTIONS];
+        outputScores = new float[1][NUM_DETECTIONS];
+        numDetections = new float[1];
+
+        Object[] inputArray = {imgData};
+        Map<Integer, Object> outputMap = new HashMap<>();
+        outputMap.put(0, outputLocations);
+        outputMap.put(1, outputClasses);
+        outputMap.put(2, outputScores);
+        outputMap.put(3, numDetections);
+        Trace.endSection();
+
+        // Run the inference call.
+        Trace.beginSection("run");
+        tfLite.runForMultipleInputsOutputs(inputArray, outputMap);
+        Trace.endSection();
+
+        // Show the best detections.
+        // after scaling them back to the input size.
+        final ArrayList<Object> recognitions = new ArrayList<Object>(NUM_DETECTIONS);
+        for (int i = 0; i < NUM_DETECTIONS; ++i) {
+            final RectF detection =
+                    new RectF(
+                            outputLocations[0][i][1] * inputSize,
+                            outputLocations[0][i][0] * inputSize,
+                            outputLocations[0][i][3] * inputSize,
+                            outputLocations[0][i][2] * inputSize);
+            // SSD Mobilenet V1 Model assumes class 0 is background class
+            // in label file and class labels start from 1 to number_of_classes+1,
+            // while outputClasses correspond to class index from 0 to number_of_classes
+            int labelOffset = 1;
+            System.out.println("" + i);
+            System.out.println(labels.get(1));
+            System.out.println(outputScores[0][i]);
+            System.out.println(detection);
+        }
+
+        Trace.endSection(); // "recognizeImage"
+        return recognitions;
+    }
+
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
-        /*lastFrame.set(Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565));
-        tfodProcessor = new TfodProcessor.Builder()
-                .setMaxNumRecognitions(10)
-                .setUseObjectTracker(true)
-                .setTrackerMaxOverlap((float) 0.2)
-                .setTrackerMinSize(16)
-                .build();
+        lastFrame.set(Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565));
         if (USE_WEBCAM) {
             visionPortal = VisionPortal.easyCreateWithDefaults(
-                    hardwareMap.get(WebcamName.class, "Webcam 1"), tfod);
+                    hardwareMap.get(WebcamName.class, "Webcam 1"));
         } else {
             visionPortal = VisionPortal.easyCreateWithDefaults(
-                    BuiltinCameraDirection.BACK, tfod);
-        }*/
+                    BuiltinCameraDirection.BACK);
+        }
+
     }
 
     @Override
@@ -121,7 +208,8 @@ public class BlockPipelineVP implements VisionProcessor, CameraStreamSource {
     }
 
     @Override
-    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx,
+            float scaleCanvasDensity, Object userContext) {
 
     }
 }
